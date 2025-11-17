b4fe1a32c28844c05a227e1cdd7b7953250e6e1f
Inish Codes <inishbista20@gmail.com>
2025-11-17 16:31:34 -0500

    Fix : Update CORS

--- app/main.py
+++ app/main.py
@@ -12,8 +12,10 @@ from fastapi.middleware.cors import CORSMiddleware
 app. add_middleware (
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
+
 )
 
 import  json
